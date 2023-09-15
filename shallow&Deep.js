69d1fa251234e9bccc017e611339a5ffb198825e
Amil Ali <66510886+amilali@users.noreply.github.com>
2023-09-15 18:57:34 +0530

    Update shallow&Deep.js

--- shallow&Deep.js
+++ shallow&Deep.js
@@ -1,4 +1,5 @@
-// Shallow copy 
+// Shallow copy
+// copies references of object not actual value of object.
 // [], {}, () --> these three are object in js which follows shallow copy
 
 let amil =  [1,2,3];
@@ -13,6 +14,7 @@ console.log(amil); //[ 'Amil', 2, 3 ] ---> shallow copy
 
 
 // Deep Copy
+// it copies actual value, deepcopy apply only on primitives not objects.
 // it is done with primitives like : Strings, booleans , number etc.
 
 let amil1 = "Amil";
