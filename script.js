e1aae5dfcf0c448fdd9081ad8e670606ad73d820
Amil Ali <amil13985@gmail.com>
2023-09-15 18:48:10 +0530

    add

---
+++ script.js
@@ -0,0 +1,22 @@
+// Shallow copy 
+// [], {}, () --> these three are object in js which follows shallow copy
+
+let amil =  [1,2,3];
+
+let ali = new Array();
+
+ali = amil; // --> it copies reference not actual object value
+
+ali[0] = "Amil";
+
+console.log(amil); //[ 'Amil', 2, 3 ] ---> shallow copy
+
+
+// Deep Copy
+// it is done with primitives like : Strings, booleans , number etc.
+
+let amil1 = "Amil";
+
+let amil2 = amil1;
+amil2 = 65; //65  --> deep copy
+console.log(amil2);
